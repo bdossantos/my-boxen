c8d80148d2f5094279bf455bc16aa053efce5ca0
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-08-23 22:40:22 +0200

    Intall zsh-syntax-highlighting package

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -56,6 +56,7 @@ class people::bdossantos {
 
   $useful = [
     'wget', 'ruby-build', 'htop-osx', 'curl', 'ssh-copy-id', 'pwgen', 'gsl',
+    'zsh-syntax-highlighting',
   ]
 
   package { $useful:
