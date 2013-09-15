bb421495f587764af891972392eca093be7585fa
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-09-15 12:05:30 +0200

    Ensure 'moreutils' is installed

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -112,7 +112,7 @@ class people::bdossantos {
   $useful = [
     'wget', 'ruby-build', 'htop-osx', 'curl', 'ssh-copy-id', 'pwgen', 'gsl',
     'zsh-syntax-highlighting', 'watch', 'stow', 'closure-compiler',
-    'htmlcompressor',
+    'htmlcompressor', 'moreutils',
   ]
 
   package { $useful:
