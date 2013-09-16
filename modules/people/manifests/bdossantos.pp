a3257cd43c113f2ec3a9551eda6a5405e9a4d174
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-09-16 15:19:37 +0200

    Ensure netcat is installed

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -112,7 +112,7 @@ class people::bdossantos {
   $useful = [
     'wget', 'ruby-build', 'htop-osx', 'curl', 'ssh-copy-id', 'pwgen', 'gsl',
     'zsh-syntax-highlighting', 'watch', 'stow', 'closure-compiler',
-    'htmlcompressor', 'moreutils',
+    'htmlcompressor', 'moreutils', 'netcat',
   ]
 
   package { $useful:
