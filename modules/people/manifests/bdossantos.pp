0078ed3fa47f224ec1083b28823f1baabe67ac10
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-09-21 10:21:37 +0200

    Install colordiff

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -113,7 +113,7 @@ class people::bdossantos {
   $useful = [
     'wget', 'ruby-build', 'htop-osx', 'curl', 'ssh-copy-id', 'pwgen', 'gsl',
     'zsh-syntax-highlighting', 'watch', 'stow', 'closure-compiler', 'pv',
-    'htmlcompressor', 'moreutils', 'netcat',
+    'htmlcompressor', 'moreutils', 'netcat', 'colordiff',
   ]
 
   package { $useful:
