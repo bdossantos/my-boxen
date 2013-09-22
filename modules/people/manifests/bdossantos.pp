6b557a7e2292952702130fe2b2ca8a6663124d49
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2013-09-23 01:13:04 +0200

    Ensure nmap is installed

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -113,7 +113,7 @@ class people::bdossantos {
   $useful = [
     'wget', 'ruby-build', 'htop-osx', 'curl', 'ssh-copy-id', 'pwgen', 'gsl',
     'zsh-syntax-highlighting', 'watch', 'stow', 'closure-compiler', 'pv',
-    'htmlcompressor', 'moreutils', 'netcat', 'colordiff', 'jq',
+    'htmlcompressor', 'moreutils', 'netcat', 'nmap', 'colordiff', 'jq',
   ]
 
   package { $useful:
