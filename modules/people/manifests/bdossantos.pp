1851fb4627de10eab2aec4d5693b0336e61768f8
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-01-13 11:18:11 +0100

    Ensure tig is installed.

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -155,7 +155,7 @@ class people::bdossantos {
     'htmlcompressor', 'moreutils', 'netcat', 'nmap', 'colordiff', 'jq',
     'ncftp', 'spark', 'battery', 'tcpdump', 'cmake', 'readline', 'gnu-sed',
     'tree', 'pigz', 'reattach-to-user-namespace', 'tmux-mem-cpu-load',
-    'zopfli',
+    'zopfli', 'tig',
   ]
 
   homebrew::tap { 'Goles/battery': } ->
