e2dd9024cb645d7d33876d6dcb4704ebe8de6954
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-10-01 23:43:23 +0200

    Ensure `z` and `v` are installed

--- modules/people/manifests/bdossantos.pp
+++ modules/people/manifests/bdossantos.pp
@@ -244,7 +244,7 @@ class people::bdossantos {
     'tree', 'pigz', 'reattach-to-user-namespace', 'tmux-mem-cpu-load',
     'zopfli', 'tig', 'the_silver_searcher', 'autojump', 'cloc', 'ipcalc',
     'awscli', 'tag', 'cheat', 'tmux-cssh', 'graphviz', 'peco', 'ansifilter',
-    'sshrc',
+    'sshrc', 'z', 'v',
   ]
 
   homebrew::tap { ['Goles/battery', 'peco/peco', ]: } ->
